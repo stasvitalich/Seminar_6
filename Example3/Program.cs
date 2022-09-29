aaba1bf5707eca347b52a42cb73142c1dabc715f
Stanislav <stasvitalich1994@gmail.com>
2022-09-29 18:20:28 +0600

    From decimal to binary, from integer to string

--- Example3/Program.cs
+++ Example3/Program.cs
@@ -1,47 +1,28 @@
 ﻿//Задача 40
 //Напишите программу, которая будет преобразовывать 
 //десятичное число в двоичное
+
+string ReverseString(string s)
+{
+    char[] arr = s.ToCharArray();
+    Array.Reverse(arr);
+    return new string(arr);
+}
+
+string GetToInt2(int number)
+{
+    string result = null;
+    while (number > 0)
+    {
+        result = result + (number % 2).ToString();
+        number = number / 2;
+    }
+    result = ReverseString(result);
+    return result;
+}
+
 Console.WriteLine("Введите десятичное число: ");
 int number = int.Parse(Console.ReadLine());
 
-
-if (number % 2 == 1)
-{
-    Console.Write(1);
-    while (number > 1)
-    {
-        number = number / 2;
-        if (number % 2 == 1)
-        {
-            Console.Write(1);
-        }
-        else
-        {
-            Console.Write(0);
-        }
-    }
-}
-
-
-
-
-
-
-if (number % 2 == 0)
-{
-    Console.Write(0);
-    while (number > 1)
-    {
-        number = number / 2;
-        if (number % 2 == 1)
-        {
-            Console.Write(1);
-        }
-        else
-        {
-            Console.Write(0);
-        }
-    }
-}
-
-
+string result = GetToInt2(number);
+Console.WriteLine(result);
