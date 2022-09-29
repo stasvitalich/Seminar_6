d20dd5ae7462fe55c16e00b7dc8810eddfe8b573
Stanislav <stasvitalich1994@gmail.com>
2022-09-29 22:33:13 +0600

    Convert to double + intersection of two straight lines

--- TASK43/Program.cs
+++ TASK43/Program.cs
@@ -4,4 +4,18 @@
 // значения b1, k1, b2 и k2 задаются пользователем.
 // Пример: b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
-Console.WriteLine("Hello, World!");
+// Инициируем ввод переменных и сразу сконвертируем их в double.
+Console.Write("Введите b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+
+// При помощи простейших действий, согласно формуле,
+// поочерёдно выведем сначала х, затем у.
+double x = -(b1 - b2) / (k1 - k2);
+double y = k1 * x + b1;
+Console.WriteLine($"Координаты точки пересечения: ({x}; {y})");
