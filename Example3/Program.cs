853f3d805114d25841a97f26f3bdcb817da94035
Stanislav <stasvitalich1994@gmail.com>
2022-09-29 15:11:23 +0600

    First part of task 40 has been resolved

--- Example3/Program.cs
+++ Example3/Program.cs
@@ -1,1 +1,47 @@
-﻿Console.WriteLine("Hello, World!");
+﻿//Задача 40
+//Напишите программу, которая будет преобразовывать 
+//десятичное число в двоичное
+Console.WriteLine("Введите десятичное число: ");
+int number = int.Parse(Console.ReadLine());
+
+
+if (number % 2 == 1)
+{
+    Console.Write(1);
+    while (number > 1)
+    {
+        number = number / 2;
+        if (number % 2 == 1)
+        {
+            Console.Write(1);
+        }
+        else
+        {
+            Console.Write(0);
+        }
+    }
+}
+
+
+
+
+
+
+if (number % 2 == 0)
+{
+    Console.Write(0);
+    while (number > 1)
+    {
+        number = number / 2;
+        if (number % 2 == 1)
+        {
+            Console.Write(1);
+        }
+        else
+        {
+            Console.Write(0);
+        }
+    }
+}
+
+
